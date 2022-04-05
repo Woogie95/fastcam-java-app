8040028d2afdf485ae3a5a34dd889613e5e6c597
Woogie95 <choisw950928@naver.com>
2022-04-06 03:20:41 +0900

    Initial commit

---
+++ src/chapter03/Exam2.java
@@ -0,0 +1,18 @@
+package chapter03;
+
+public class Exam2 {
+    public static void main(String[] args) {
+        // num1이 가진 숫자와 num2가 가진 숫자를 교환하는 프로그램
+
+        int num1 = 4;
+        int num2 = 44;
+        int temp = 0;
+
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        System.out.println(num1); // 44
+        System.out.println(num2); // 4
+    }
+}
---
+++ src/chapter03/ForTest2.java
@@ -0,0 +1,11 @@
+package chapter03;
+
+public class ForTest2 {
+    public static void main(String[] args) {
+
+
+        for (int i = 2; i <= 9 ; i++) {
+
+        }
+    }
+}
