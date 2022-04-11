6474bc03b4c3d9189ebb5217ed4de4289c641fbc
Woogie95 <choisw950928@gmail.com>
2022-04-12 00:21:13 +0900

    feat: Example 상속

---
+++ src/backendlecture/Chapter3/ch02/VipCustomer.java
@@ -0,0 +1,17 @@
+package backendlecture.Chapter3.ch02;
+
+public class VipCustomer  extends Customer{
+
+    double salesRatio;
+    private String agentID;
+
+    public VipCustomer() {
+        bonusRatio = 0.05;
+        salesRatio = 0.1;
+        customerGrade = "VIP";
+    }
+
+    public String getAgentID() {
+        return agentID;
+    }
+}
---
+++ src/backendlecture/Chapter3/ch04/VipCustomer.java
@@ -0,0 +1,19 @@
+//package backendlecture.Chapter3.ch04;
+//
+//public class VipCustomer  extends Customer {
+//
+//    double salesRatio;
+//    private String agentID;
+//
+//    public VipCustomer(int customerID, String customerName){
+//        super(customerID, customerName);
+//
+//        customerGrade = "VIP";
+//        bonusRatio = 0.05;
+//        salesRatio = 0.1;
+//    }
+//
+//    public String getAgentID() {
+//        return agentID;
+//    }
+//}
